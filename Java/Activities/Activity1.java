c365c15bb92eed953aa83dcbd6d89029a66c5025
jenosamuel7 <jeno.samuel.j@ibm.com>
2024-06-10 13:11:22 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,19 @@
+package Activities;
+
+public class Activity1 {
+
+
+        public static void main(String[] args) {
+            Car CarName = new Car();
+            CarName.make = 2014;
+            CarName.color = "Black";
+            CarName.transmission = "Manual";
+        
+            
+            CarName.displayCharacterstics();
+            CarName.accelerate();
+            CarName.brake();
+        }
+    
+    }
+
