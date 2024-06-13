3e0dcb17a696accac61f05c9862dd0f69afd6e7f
SenthilkumarSChn <senthilkumar.s@ibm.com>
2024-06-13 12:54:50 +0530

    Update Activity8.java

--- Java/Activities/Activity8.java
+++ Java/Activities/Activity8.java
@@ -16,9 +16,9 @@ class CustomException extends Exception {
 public class Activity8 {
     public static void main(String[] a){
         try {
-            // Method call with correct input
+           
             Activity8.exceptionTest("Will print to console");
-            // Method call with incorrect input
+           
             Activity8.exceptionTest(null); // Exception is thrown here
             Activity8.exceptionTest("Won't execute");
         } catch(CustomException mae) {
