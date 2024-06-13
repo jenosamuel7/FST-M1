8d5fb4e70d7ebf9d58bcee91249974f349e4317e
SenthilkumarSChn <senthilkumar.s@ibm.com>
2024-06-13 12:50:06 +0530

    Update Activity4.py

--- Python/Activities/Activity4.py
+++ Python/Activities/Activity4.py
@@ -1,14 +1,14 @@
 	
-# Get the names of the users
+
 user1 = input("What is Player 1's name? ")
 user2 = input("What is Player 2's name? ")
  
-# While looping endlessly
+
 while True:
-    # Ask User1's choice
+    
     user1_answer = input(user1 + ", do you want to choose rock, paper or scissors? ").lower()
     
-    # Ask User2's choice
+   
     user2_answer = input(user2 + ", do you want to choose rock, paper or scissors? ").lower()
     
     # Run the algorithm to see who wins
@@ -32,16 +32,16 @@ while True:
     else:
         print("Invalid input! You have not entered rock, paper or scissors, try again.")
  
-    # Ask them if they want to play again
+   
     repeat = input("Do you want to play another round? Yes/No: ").lower()
     
-    # If they say yes, don't do anything
+  
     if(repeat == "yes"):
         pass
-    # If they say no, exit the game
+  
     elif(repeat == "no"):
         raise SystemExit
-    # If they say anything else, exit with an error message.
+  
     else:
         print("You entered an invalid option. Exiting now.")
         raise SystemExit
