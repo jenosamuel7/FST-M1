767f9f554449ceb415ec2eaa224a6163dac181fc
jenosamuel7 <jeno.samuel.j@ibm.com>
2024-07-15 16:48:51 +0530

    Add files via upload

---
+++ Java/Activities/Plane.java
@@ -0,0 +1,12 @@
+package Activities.Activities6;
+
+
+import java.util.Date;
+import java.util.List;
+
+ class Plane {
+    public List<String> passengers;
+    public int maxPassengers;
+    public Date lastTimeTookOf;
+    public Date lastTimeLanded;
+}
